78335ac863d488386ac29c1a31941f6626b4a53b
Alexandre <alexandre.costa22@outlook.com>
2025-05-19 19:18:48 -0300

    .

--- src/app/_components/main-page/main-page.component.ts
+++ src/app/_components/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit  } from '@angular/core';
 import { ExpeditionService } from '../../services/expeditions.service';
 import { Expeditions } from '../../models/expeditions.model';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 declare var bootstrap: any; // isso expõe a instância JS do Bootstrap
 
@@ -15,9 +16,13 @@ export class MainPageComponent implements AfterViewInit {
   lastExpeditions: Expeditions[] = [];
   crewImages: string[] = [];
   expeditionImages: { [name: string]: string } = {};
+  private genAI: GoogleGenerativeAI;
 
 
-  constructor(private expeditionService: ExpeditionService) { }
+  constructor(private expeditionService: ExpeditionService
+  ) { 
+    this.genAI = new GoogleGenerativeAI('AIzaSyC6Vd9f4ta2kLLRmgnwBwZF7jYpoYYWYYk');
+  }
 
 ngOnInit() {
   this.expeditionService.getExpeditions().subscribe(data => {
@@ -36,6 +41,21 @@ ngOnInit() {
       });
     });
   });
+  this.buscaTeste('Expedição 71');
+}
+
+async buscaTeste(prompt: string) {
+  let busca = 'Utilizando fontes confiáveis e o mais precisas possivel, me retorne uma ddescrição didática sobre ' + prompt+'Caso não existam informações confiáveis, apenas retorne "Dados não encontraos"'
+  const model = this.genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+
+  const result = await model.generateContent(busca);
+  const response = await result.response;
+  const text = response.text();
+
+  console.log(text)
+
+  return text;
 }
 
 ngAfterViewInit(): void {
