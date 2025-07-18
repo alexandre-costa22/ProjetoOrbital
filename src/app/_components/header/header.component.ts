158e19c8af4ffcbf61b015a8a4eff713662159dc
Raul Silveira <raulantsilveira@gmail.com>
2025-07-18 09:56:14 -0300

    ajustes de responsividade

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Auth, onAuthStateChanged, signOut, User } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
@@ -14,6 +14,9 @@ export class HeaderComponent  {
   currentUser: User | null = null;
   router: Router = inject(Router);
 
+  isMenuOpen = false;
+  isSearchOpen = false;
+
   constructor(private auth: Auth) { }
 
   ngOnInit() {
@@ -21,25 +24,39 @@ export class HeaderComponent  {
       if (user) {
         this.isAuth = true;
         this.currentUser = user;
-        localStorage.setItem('credencial', JSON.stringify({ email: user.email, uid: user.uid }));
       } else {
         this.isAuth = false;
         this.currentUser = null;
-        localStorage.removeItem('credencial');
       }
     });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) this.isSearchOpen = false;
+  }
+
+  toggleSearch(): void {
+    this.isSearchOpen = !this.isSearchOpen;
+    if (this.isSearchOpen) this.isMenuOpen = false;
+  }
+
+  closeMenuAndNavigate() {
+    this.isMenuOpen = false;
+  }
+
+  logOutAndCloseMenu() {
+    this.logOut();
+    this.closeMenuAndNavigate();
   }
 
   logOut() {
     signOut(this.auth)
       .then(() => {
-        this.isAuth = false;
-        localStorage.removeItem('credencial');
         this.router.navigate(['/main']);
       })
       .catch((err) => {
         console.error("Erro ao deslogar:", err);
       });
   }
-
 }
--- src/app/_components/main-page/main-page.component.ts
+++ src/app/_components/main-page/main-page.component.ts
@@ -32,5 +32,5 @@ export class MainPageComponent implements AfterViewInit {
   }
 
 
-    
+
 }
